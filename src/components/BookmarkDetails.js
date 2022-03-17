04804cc7085d89778a5f85232e470cc63ec2f7cd
jluna101 <jfluna1996@gmail.com>
2022-03-17 15:28:50 -0700

    Added components and installed router dom

---
+++ src/components/BookmarkDetails.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function BookmarkDetails(props) {
+    return (
+        <div>
+            Hello from BMD
+        </div>
+    );
+}
+
+export default BookmarkDetails;
